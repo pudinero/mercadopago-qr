cfa551ce45e57328df60546ce682a74afe321d6f
Cristian Leguizamón <cristianleguizamon37@gmail.com>
2020-06-26 17:19:58 -0300

    Port issue

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 
@@ -9,6 +11,6 @@ app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
